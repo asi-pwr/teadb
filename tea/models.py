4ce4d43cb22c5b9edac27c5638cbeeceda5754d2
Łukasz Bieszczad <Luszak@localhost.localdomain>
2016-02-24 22:34:44 +0100

    Change models: Tea, TeaType and add __str__ method

--- tea/models.py
+++ tea/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
 class TeaType(models.Model):
-    tea_choices=(('a','Czarna'),('b','Zielona'),('c','Czerwona'),('d','Oolong'),('e','Biala'),('f','Yerba mate'),('g','Ziolowa'),('h','Owocowe'),('i','Krzewow i kory'),)
-    name=models.CharField(max_length=1,choices=tea_choices)
+    name=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 
 class Tea(models.Model):
     name=models.ForeignKey(TeaType)
     description=models.TextField()
     region=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name.name
 
 
 class Review(models.Model):
