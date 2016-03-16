23d24f4a4850666ceba0046bea4af3342eb240e0
Łukasz Bieszczad <Luszak@localhost.localdomain>
2016-03-16 21:00:27 +0100

    Fixed review view

--- tea/views.py
+++ tea/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import ListView, DetailView, CreateView
 from tea.models import Tea, Review
 
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
     template_name = "tea/index.html"
     model = Tea
 
 
-class TeaDetailView(generic.DetailView):
+class TeaDetailView(DetailView):
     template_name = "tea/tea_detail.html"
     model = Tea
 
 
-class ReviewView(generic.ListView):
+class ReviewView(ListView):
     template_name = 'tea/tea_reviews.html'
     model = Review
+
+
+class CreateReviewView(CreateView):
+    template_name = 'tea/create_review.html'
+    model = Review
+    fields = ['content','author']
--- teadb/urls.py
+++ teadb/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from tea.views import IndexView, ReviewView, TeaDetailView
+from tea.views import IndexView, ReviewView, TeaDetailView, CreateReviewView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
