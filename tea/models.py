f0526a67ee1b9654a5e3af181d1d9136467c711a
Piotr Skorupa <ptrskorupa@gmail.com>
2016-05-25 20:56:29 +0200

    Revert "Add Region model"
    This reverts commit aa3b33b51a6b92d023516641b712ba39688a75c7.

--- tea/models.py
+++ tea/models.py
@@ -6,19 +6,11 @@ class TeaType(models.Model):
     def __str__(self):
         return "%s" % self.tea_type
 
-
-class Region(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
 class Tea(models.Model):
     name = models.CharField(max_length=50)
     type = models.ForeignKey(TeaType)
     description = models.TextField()
-    region = models.ForeignKey(Region)
+    region = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
